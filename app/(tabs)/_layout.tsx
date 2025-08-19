b71140cfc5ce15a9e4d8e8058eeef4ce3e79cece
Usama <uelareeny@gmail.com>
2025-08-19 16:48:05 +0300

    Added Layouts and icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
-import { Text } from "react-native";
-
+import { View } from "react-native";
 export default function TabLayout() {
   return (
     <Tabs
@@ -13,35 +13,78 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: () => <Text>🏠</Text>,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="friends"
         options={{
           title: "Friends",
-          tabBarIcon: () => <Text>👥</Text>,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "people-sharp" : "people-outline"}
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="camera"
         options={{
-          title: "Camera",
-          tabBarIcon: () => <Text>👥</Text>,
+          title: "",
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                width: 75,
+                height: 75,
+                justifyContent: "center",
+                alignItems: "center",
+                marginBottom: 20,
+              }}
+            >
+              <Ionicons
+                name={focused ? "add-circle-outline" : "add-circle-sharp"}
+                size={75}
+                color="black"
+              />
+            </View>
+          ),
         }}
-      />{" "}
+      />
+
       <Tabs.Screen
         name="inbox"
         options={{
           title: "Inbox",
-          tabBarIcon: () => <Text>👥</Text>,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={
+                focused ? "chatbox-ellipses-sharp" : "chatbox-ellipses-outline"
+              }
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
-          tabBarIcon: () => <Text>👥</Text>,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "person-sharp" : "person-outline"}
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,8 +1,8 @@
 import { Text, View } from "react-native";
 export default function Camera() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-lg">Camera</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-black text-lg">Camera</Text>
     </View>
   );
 }
